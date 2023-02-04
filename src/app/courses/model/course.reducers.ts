9c777b8b765ecf561363a742ed7167d1455f0645
Kenneth Keim <32063766+kennethkeim@users.noreply.github.com>
2023-02-04 13:33:28 -0500

    sort courses

--- src/app/courses/model/course.reducers.ts
+++ src/app/courses/model/course.reducers.ts
@@ -1,11 +1,13 @@
 import { createEntityAdapter, EntityState } from "@ngrx/entity";
 import { createReducer, on } from "@ngrx/store";
 import { CourseActions } from "../shared/action-types";
-import { Course } from "./course";
+import { compareCourses, Course } from "./course";
 
 export interface CoursesState extends EntityState<Course> {}
 
-export const adapter = createEntityAdapter<Course>();
+export const adapter = createEntityAdapter<Course>({
+  sortComparer: compareCourses,
+});
 
 export const initialCoursesState = adapter.getInitialState();
 
