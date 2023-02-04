156ece5a157a8a92bd557680cd0c68bedfaa87d4
Kenneth Keim <32063766+kennethkeim@users.noreply.github.com>
2023-02-04 13:33:27 -0500

    simplify logger

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -19,7 +19,7 @@ export const reducers: ActionReducerMap<AppState> = {
 export function logger(reducer: ActionReducer<any>): ActionReducer<any> {
   return (state, action) => {
     // console.log("state before", state);
-    console.log("action", action.type);
+    console.log(action.type);
     return reducer(state, action);
   };
 }
